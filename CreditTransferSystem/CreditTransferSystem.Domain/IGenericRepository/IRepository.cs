f36dca49399090f342d7f9b7bd2fc4e66444958d
Leen Odeh <leenodeh287@gmail.com>
2024-03-02 16:24:14 +0300

    add IGenericRepo & GenericRepo  then Implement to use this in controller

---
+++ CreditTransferSystem/CreditTransferSystem.Domain/IGenericRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditTransferSystem.Domain.IGenericRepository
+{
+    public interface IRepository <T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity, int id);
+        Task<T> DeleteAsync(int id);
+    }
+}
---
+++ CreditTransferSystem/CreditTransferSystem.Infrastructure/GenericRepository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using CreditTransferSystem.Domain.IGenericRepository;
+using CreditTransferSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CreditTransferSystem.Infrastructure.GenericRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity == null)
+            {
+                throw new InvalidOperationException("Entity not found");
+            }
+
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> UpdateAsync(T entity, int id)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
